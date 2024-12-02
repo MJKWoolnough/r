3a4ce12c4201f94a3c48278005ebc55258719d87
Michael Woolnough <michael.woolnough@gmail.com>
2024-12-02 19:24:58 +0000

    added Tokeniser test structure

---
+++ tokeniser_test.go
@@ -0,0 +1,35 @@
+package r
+
+import (
+	"testing"
+
+	"vimagination.zapto.org/parser"
+)
+
+func TestTokeniser(t *testing.T) {
+	for n, test := range [...]struct {
+		Input  string
+		Output []parser.Token
+	}{} {
+		p := parser.NewStringTokeniser(test.Input)
+
+		SetTokeniser(&p)
+
+		for m, tkn := range test.Output {
+			tk, _ := p.GetToken()
+			if tk.Type != tkn.Type {
+				if tk.Type == parser.TokenError {
+					t.Errorf("test %d.%d: unexpected error: %s", n+1, m+1, tk.Data)
+				} else {
+					t.Errorf("test %d.%d: Incorrect type, expecting %d, got %d", n+1, m+1, tkn.Type, tk.Type)
+				}
+
+				break
+			} else if tk.Data != tkn.Data {
+				t.Errorf("test %d.%d: Incorrect data, expecting %q, got %q", n+1, m+1, tkn.Data, tk.Data)
+
+				break
+			}
+		}
+	}
+}
