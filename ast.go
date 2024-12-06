5934d9c69cf6ac626c86278998a6f1f474914f42
Michael Woolnough <michael.woolnough@gmail.com>
2024-12-06 18:22:58 +0000

    added primary File parsing func

---
+++ ast.go
@@ -0,0 +1,42 @@
+package r
+
+import "vimagination.zapto.org/parser"
+
+type File struct {
+	Statements []Expression
+	Tokens     Tokens
+}
+
+func Parse(t Tokeniser) (*File, error) {
+	r, err := newRParser(t)
+	if err != nil {
+		return nil, err
+	}
+
+	f := new(File)
+	if err = f.parse(r); err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+func (f *File) parse(r rParser) error {
+	for r.AcceptRunWhitespace() != parser.TokenDone {
+		var s Expression
+
+		q := r.NewGoal()
+
+		if err := s.parse(q); err != nil {
+			return r.Error("File", err)
+		}
+
+		f.Statements = append(f.Statements, s)
+
+		r.Score(q)
+	}
+
+	f.Tokens = r.ToTokens()
+
+	return nil
+}
---
+++ ast_expression.go
@@ -0,0 +1,7 @@
+package r
+
+type Expression struct{}
+
+func (e *Expression) parse(r rParser) error {
+	return nil
+}
