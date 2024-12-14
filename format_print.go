db7cb48431052daa7ede14beec5995db9d4bd0e2
Michael Woolnough <michael.woolnough@gmail.com>
2024-12-14 19:31:59 +0000

    add printSource stubs

---
+++ format_print.go
@@ -0,0 +1,63 @@
+package r
+
+import "io"
+
+func (a AdditionExpression) printSource(w io.Writer, v bool) {}
+
+func (a AndExpression) printSource(w io.Writer, v bool) {}
+
+func (a Arg) printSource(w io.Writer, v bool) {}
+
+func (a ArgList) printSource(w io.Writer, v bool) {}
+
+func (a Argument) printSource(w io.Writer, v bool) {}
+
+func (a AssignmentExpression) printSource(w io.Writer, v bool) {}
+
+func (c Call) printSource(w io.Writer, v bool) {}
+
+func (c CompoundExpression) printSource(w io.Writer, v bool) {}
+
+func (e ExponentiationExpression) printSource(w io.Writer, v bool) {}
+
+func (e Expression) printSource(w io.Writer, v bool) {}
+
+func (f FlowControl) printSource(w io.Writer, v bool) {}
+
+func (f ForControl) printSource(w io.Writer, v bool) {}
+
+func (f FormulaeExpression) printSource(w io.Writer, v bool) {}
+
+func (f FunctionDefinition) printSource(w io.Writer, v bool) {}
+
+func (i IfControl) printSource(w io.Writer, v bool) {}
+
+func (i Index) printSource(w io.Writer, v bool) {}
+
+func (i IndexOrCallExpression) printSource(w io.Writer, v bool) {}
+
+func (m MultiplicationExpression) printSource(w io.Writer, v bool) {}
+
+func (n NotExpression) printSource(w io.Writer, v bool) {}
+
+func (o OrExpression) printSource(w io.Writer, v bool) {}
+
+func (p PipeOrSpecialExpression) printSource(w io.Writer, v bool) {}
+
+func (q QueryExpression) printSource(w io.Writer, v bool) {}
+
+func (r RelationalExpression) printSource(w io.Writer, v bool) {}
+
+func (r RepeatControl) printSource(w io.Writer, v bool) {}
+
+func (s ScopeExpression) printSource(w io.Writer, v bool) {}
+
+func (s SequenceExpression) printSource(w io.Writer, v bool) {}
+
+func (s SimpleExpression) printSource(w io.Writer, v bool) {}
+
+func (s SubsetExpression) printSource(w io.Writer, v bool) {}
+
+func (u UnaryExpression) printSource(w io.Writer, v bool) {}
+
+func (wc WhileControl) printSource(w io.Writer, v bool) {}
