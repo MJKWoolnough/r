02740a9495db4f4745c7a85e99f342608d7aff4a
Michael Woolnough <michael.woolnough@gmail.com>
2024-12-15 17:46:10 +0000

    add printType method for Token(s)

--- format.go
+++ format.go
@@ -1,6 +1,143 @@
 package r
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"vimagination.zapto.org/parser"
+)
+
+var indent = []byte{'\t'}
+
+type indentPrinter struct {
+	io.Writer
+}
+
+func (i *indentPrinter) Write(p []byte) (int, error) {
+	var (
+		total int
+		last  int
+	)
+
+	for n, c := range p {
+		if c == '\n' {
+			m, err := i.Writer.Write(p[last : n+1])
+			total += m
+
+			if err != nil {
+				return total, err
+			}
+
+			_, err = i.Writer.Write(indent)
+			if err != nil {
+				return total, err
+			}
+
+			last = n + 1
+		}
+	}
+
+	if last != len(p) {
+		m, err := i.Writer.Write(p[last:])
+		total += m
+
+		if err != nil {
+			return total, err
+		}
+	}
+
+	return total, nil
+}
+
+func (i *indentPrinter) Print(args ...interface{}) {
+	fmt.Fprint(i, args...)
+}
+
+func (i *indentPrinter) Printf(format string, args ...interface{}) {
+	fmt.Fprintf(i, format, args...)
+}
+
+func (i *indentPrinter) WriteString(s string) (int, error) {
+	return i.Write([]byte(s))
+}
+
+func (t Token) printType(w io.Writer, v bool) {
+	var typ string
+
+	switch t.Type {
+	case TokenWhitespace:
+		typ = "Whitespace"
+	case TokenLineTerminator:
+		typ = "LineTerminator"
+	case TokenExpressionTerminator:
+		typ = "ExpressionTerminator"
+	case TokenComment:
+		typ = "Comment"
+	case TokenStringLiteral:
+		typ = "StringLiteral"
+	case TokenNumericLiteral:
+		typ = "NumericLiteral"
+	case TokenIntegerLiteral:
+		typ = "IntegerLiteral"
+	case TokenComplexLiteral:
+		typ = "ComplexLiteral"
+	case TokenBooleanLiteral:
+		typ = "BooleanLiteral"
+	case TokenNull:
+		typ = "Null"
+	case TokenNA:
+		typ = "NA"
+	case TokenIdentifier:
+		typ = "Identifier"
+	case TokenKeyword:
+		typ = "Keyword"
+	case TokenEllipsis:
+		typ = "Ellipsis"
+	case TokenOperator:
+		typ = "Operator"
+	case TokenSpecialOperator:
+		typ = "SpecialOperator"
+	case TokenGrouping:
+		typ = "Grouping"
+	case parser.TokenDone:
+		typ = "Done"
+	case parser.TokenError:
+		typ = "Error"
+	default:
+		typ = "Unknown"
+	}
+
+	fmt.Fprintf(w, "Type: %s - Data: %q", typ, t.Data)
+
+	if v {
+		fmt.Fprintf(w, " - Position: %d (%d: %d)", t.Pos, t.Line, t.LinePos)
+	}
+}
+
+func (t Tokens) printType(w io.Writer, v bool) {
+	if t == nil {
+		io.WriteString(w, "nil")
+
+		return
+	}
+
+	if len(t) == 0 {
+		io.WriteString(w, "[]")
+
+		return
+	}
+
+	io.WriteString(w, "[")
+
+	ipp := indentPrinter{w}
+
+	for n, t := range t {
+		ipp.Printf("\n%d: ", n)
+		t.printType(w, v)
+	}
+
+	io.WriteString(w, "\n]")
+}
 
 func (a AdditionType) String() string {
 	switch a {
