5c88f43e49a311648c0a9d12fd8570553eb3e19f
Michael Woolnough <michael.woolnough@gmail.com>
2024-12-16 20:34:15 +0000

    assign types that will be wrapped/unwrapped with a util fn

---
+++ wrappable.go
@@ -0,0 +1,46 @@
+package r
+
+type QueryWrappable interface {
+	Type
+	queryWrappable()
+}
+
+func (QueryExpression) queryWrappable() {}
+
+func (AssignmentExpression) queryWrappable() {}
+
+func (FormulaeExpression) queryWrappable() {}
+
+func (OrExpression) queryWrappable() {}
+
+func (AndExpression) queryWrappable() {}
+
+func (NotExpression) queryWrappable() {}
+
+func (RelationalExpression) queryWrappable() {}
+
+func (AdditionExpression) queryWrappable() {}
+
+func (MultiplicationExpression) queryWrappable() {}
+
+func (PipeOrSpecialExpression) queryWrappable() {}
+
+func (SequenceExpression) queryWrappable() {}
+
+func (UnaryExpression) queryWrappable() {}
+
+func (ExponentiationExpression) queryWrappable() {}
+
+func (SubsetExpression) queryWrappable() {}
+
+func (ScopeExpression) queryWrappable() {}
+
+func (IndexOrCallExpression) queryWrappable() {}
+
+func (Index) queryWrappable() {}
+
+func (Call) queryWrappable() {}
+
+func (SimpleExpression) queryWrappable() {}
+
+func (CompoundExpression) queryWrappable() {}
