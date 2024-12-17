bdcf79e235983b950f4a2cad606abf4d21e5bebd
Michael Woolnough <michael.woolnough@gmail.com>
2024-12-17 17:45:37 +0000

    add testing framework

---
+++ ast_test.go
@@ -0,0 +1,48 @@
+package r
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"vimagination.zapto.org/parser"
+)
+
+type sourceFn struct {
+	Source string
+	Fn     func(*test, Tokens)
+}
+
+type test struct {
+	Tokens rParser
+	Output Type
+	Err    error
+}
+
+func makeTokeniser(tk parser.Tokeniser) *parser.Tokeniser {
+	return &tk
+}
+
+func doTests(t *testing.T, tests []sourceFn, fn func(*test) (Type, error)) {
+	t.Helper()
+
+	var err error
+
+	for n, tt := range tests {
+		var ts test
+
+		if ts.Tokens, err = newRParser(makeTokeniser(parser.NewStringTokeniser(tt.Source))); err != nil {
+			t.Errorf("test %d: unexpected error: %s", n+1, err)
+
+			continue
+		}
+
+		tt.Fn(&ts, Tokens(ts.Tokens[:cap(ts.Tokens)]))
+
+		if output, err := fn(&ts); !errors.Is(err, ts.Err) {
+			t.Errorf("test %d: expecting error: %v, got %v", n+1, ts.Err, err)
+		} else if ts.Output != nil && !reflect.DeepEqual(output, ts.Output) {
+			t.Errorf("test %d: expecting \n%+v\n...got...\n%+v", n+1, ts.Output, output)
+		}
+	}
+}
