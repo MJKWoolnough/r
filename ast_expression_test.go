ddce2de725e63056d1371c021a2d34cdd17a829f
Michael Woolnough <michael.woolnough@gmail.com>
2024-12-20 21:37:37 +0000

    added ExponentiationExpression tests

--- ast_expression_test.go
+++ ast_expression_test.go
@@ -139,6 +139,103 @@ func TestCompoundExpression(t *testing.T) {
 	})
 }
 
+func TestExponentiationExpression(t *testing.T) {
+	doTests(t, []sourceFn{
+		{"a", func(t *test, tk Tokens) { // 1
+			t.Output = ExponentiationExpression{
+				SubsetExpression: SubsetExpression{
+					ScopeExpression: ScopeExpression{
+						IndexOrCallExpression: IndexOrCallExpression{
+							SimpleExpression: &SimpleExpression{
+								Identifier: &tk[0],
+								Tokens:     tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{"a^b", func(t *test, tk Tokens) { // 2
+			t.Output = ExponentiationExpression{
+				SubsetExpression: SubsetExpression{
+					ScopeExpression: ScopeExpression{
+						IndexOrCallExpression: IndexOrCallExpression{
+							SimpleExpression: &SimpleExpression{
+								Identifier: &tk[0],
+								Tokens:     tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				ExponentiationExpression: &ExponentiationExpression{
+					SubsetExpression: SubsetExpression{
+						ScopeExpression: ScopeExpression{
+							IndexOrCallExpression: IndexOrCallExpression{
+								SimpleExpression: &SimpleExpression{
+									Identifier: &tk[2],
+									Tokens:     tk[2:3],
+								},
+								Tokens: tk[2:3],
+							},
+							Tokens: tk[2:3],
+						},
+						Tokens: tk[2:3],
+					},
+					Tokens: tk[2:3],
+				},
+				Tokens: tk[:3],
+			}
+		}},
+		{"a ^ b", func(t *test, tk Tokens) { // 3
+			t.Output = ExponentiationExpression{
+				SubsetExpression: SubsetExpression{
+					ScopeExpression: ScopeExpression{
+						IndexOrCallExpression: IndexOrCallExpression{
+							SimpleExpression: &SimpleExpression{
+								Identifier: &tk[0],
+								Tokens:     tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				ExponentiationExpression: &ExponentiationExpression{
+					SubsetExpression: SubsetExpression{
+						ScopeExpression: ScopeExpression{
+							IndexOrCallExpression: IndexOrCallExpression{
+								SimpleExpression: &SimpleExpression{
+									Identifier: &tk[4],
+									Tokens:     tk[4:5],
+								},
+								Tokens: tk[4:5],
+							},
+							Tokens: tk[4:5],
+						},
+						Tokens: tk[4:5],
+					},
+					Tokens: tk[4:5],
+				},
+				Tokens: tk[:5],
+			}
+		}},
+	}, func(t *test) (Type, error) {
+		var ee ExponentiationExpression
+
+		err := ee.parse(&t.Tokens)
+
+		return ee, err
+	})
+}
+
 func TestSubsetExpression(t *testing.T) {
 	doTests(t, []sourceFn{
 		{"a", func(t *test, tk Tokens) { // 1
