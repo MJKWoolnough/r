4a39b71fff8365399859f5d081821edab7e7067d
Michael Woolnough <michael.woolnough@gmail.com>
2024-12-20 21:39:07 +0000

    added ExponentiationExpression Error tests

--- ast_expression_test.go
+++ ast_expression_test.go
@@ -227,6 +227,56 @@ func TestExponentiationExpression(t *testing.T) {
 				Tokens: tk[:5],
 			}
 		}},
+		{"in", func(t *test, tk Tokens) { // 4
+			t.Err = Error{
+				Err: Error{
+					Err: Error{
+						Err: Error{
+							Err: Error{
+								Err:     ErrInvalidSimpleExpression,
+								Parsing: "SimpleExpression",
+								Token:   tk[0],
+							},
+							Parsing: "IndexOrCallExpression",
+							Token:   tk[0],
+						},
+						Parsing: "ScopeExpression",
+						Token:   tk[0],
+					},
+					Parsing: "SubsetExpression",
+					Token:   tk[0],
+				},
+				Parsing: "ExponentiationExpression",
+				Token:   tk[0],
+			}
+		}},
+		{"a^in", func(t *test, tk Tokens) { // 5
+			t.Err = Error{
+				Err: Error{
+					Err: Error{
+						Err: Error{
+							Err: Error{
+								Err: Error{
+									Err:     ErrInvalidSimpleExpression,
+									Parsing: "SimpleExpression",
+									Token:   tk[2],
+								},
+								Parsing: "IndexOrCallExpression",
+								Token:   tk[2],
+							},
+							Parsing: "ScopeExpression",
+							Token:   tk[2],
+						},
+						Parsing: "SubsetExpression",
+						Token:   tk[2],
+					},
+					Parsing: "ExponentiationExpression",
+					Token:   tk[2],
+				},
+				Parsing: "ExponentiationExpression",
+				Token:   tk[2],
+			}
+		}},
 	}, func(t *test) (Type, error) {
 		var ee ExponentiationExpression
 
