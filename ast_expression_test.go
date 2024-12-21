8ce995b07df0d59fe6e5e5d8276e5e6cfd539c00
Michael Woolnough <michael.woolnough@gmail.com>
2024-12-21 20:56:17 +0000

    added PipeOrSpecialExpression tests

--- ast_expression_test.go
+++ ast_expression_test.go
@@ -139,6 +139,256 @@ func TestCompoundExpression(t *testing.T) {
 	})
 }
 
+func TestPipeOrSpecialExpression(t *testing.T) {
+	doTests(t, []sourceFn{
+		{"a", func(t *test, tk Tokens) { // 1
+			t.Output = PipeOrSpecialExpression{
+				SequenceExpression: SequenceExpression{
+					UnaryExpression: UnaryExpression{
+						ExponentiationExpression: ExponentiationExpression{
+							SubsetExpression: SubsetExpression{
+								ScopeExpression: ScopeExpression{
+									IndexOrCallExpression: IndexOrCallExpression{
+										SimpleExpression: &SimpleExpression{
+											Identifier: &tk[0],
+											Tokens:     tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+								Tokens: tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{"a|>b", func(t *test, tk Tokens) { // 2
+			t.Output = PipeOrSpecialExpression{
+				SequenceExpression: SequenceExpression{
+					UnaryExpression: UnaryExpression{
+						ExponentiationExpression: ExponentiationExpression{
+							SubsetExpression: SubsetExpression{
+								ScopeExpression: ScopeExpression{
+									IndexOrCallExpression: IndexOrCallExpression{
+										SimpleExpression: &SimpleExpression{
+											Identifier: &tk[0],
+											Tokens:     tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+								Tokens: tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Operator: &tk[1],
+				PipeOrSpecialExpression: &PipeOrSpecialExpression{
+					SequenceExpression: SequenceExpression{
+						UnaryExpression: UnaryExpression{
+							ExponentiationExpression: ExponentiationExpression{
+								SubsetExpression: SubsetExpression{
+									ScopeExpression: ScopeExpression{
+										IndexOrCallExpression: IndexOrCallExpression{
+											SimpleExpression: &SimpleExpression{
+												Identifier: &tk[2],
+												Tokens:     tk[2:3],
+											},
+											Tokens: tk[2:3],
+										},
+										Tokens: tk[2:3],
+									},
+									Tokens: tk[2:3],
+								},
+								Tokens: tk[2:3],
+							},
+							Tokens: tk[2:3],
+						},
+						Tokens: tk[2:3],
+					},
+					Tokens: tk[2:3],
+				},
+				Tokens: tk[:3],
+			}
+		}},
+		{"a |> b", func(t *test, tk Tokens) { // 3
+			t.Output = PipeOrSpecialExpression{
+				SequenceExpression: SequenceExpression{
+					UnaryExpression: UnaryExpression{
+						ExponentiationExpression: ExponentiationExpression{
+							SubsetExpression: SubsetExpression{
+								ScopeExpression: ScopeExpression{
+									IndexOrCallExpression: IndexOrCallExpression{
+										SimpleExpression: &SimpleExpression{
+											Identifier: &tk[0],
+											Tokens:     tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+								Tokens: tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Operator: &tk[2],
+				PipeOrSpecialExpression: &PipeOrSpecialExpression{
+					SequenceExpression: SequenceExpression{
+						UnaryExpression: UnaryExpression{
+							ExponentiationExpression: ExponentiationExpression{
+								SubsetExpression: SubsetExpression{
+									ScopeExpression: ScopeExpression{
+										IndexOrCallExpression: IndexOrCallExpression{
+											SimpleExpression: &SimpleExpression{
+												Identifier: &tk[4],
+												Tokens:     tk[4:5],
+											},
+											Tokens: tk[4:5],
+										},
+										Tokens: tk[4:5],
+									},
+									Tokens: tk[4:5],
+								},
+								Tokens: tk[4:5],
+							},
+							Tokens: tk[4:5],
+						},
+						Tokens: tk[4:5],
+					},
+					Tokens: tk[4:5],
+				},
+				Tokens: tk[:5],
+			}
+		}},
+		{"a%%b", func(t *test, tk Tokens) { // 4
+			t.Output = PipeOrSpecialExpression{
+				SequenceExpression: SequenceExpression{
+					UnaryExpression: UnaryExpression{
+						ExponentiationExpression: ExponentiationExpression{
+							SubsetExpression: SubsetExpression{
+								ScopeExpression: ScopeExpression{
+									IndexOrCallExpression: IndexOrCallExpression{
+										SimpleExpression: &SimpleExpression{
+											Identifier: &tk[0],
+											Tokens:     tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+								Tokens: tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Operator: &tk[1],
+				PipeOrSpecialExpression: &PipeOrSpecialExpression{
+					SequenceExpression: SequenceExpression{
+						UnaryExpression: UnaryExpression{
+							ExponentiationExpression: ExponentiationExpression{
+								SubsetExpression: SubsetExpression{
+									ScopeExpression: ScopeExpression{
+										IndexOrCallExpression: IndexOrCallExpression{
+											SimpleExpression: &SimpleExpression{
+												Identifier: &tk[2],
+												Tokens:     tk[2:3],
+											},
+											Tokens: tk[2:3],
+										},
+										Tokens: tk[2:3],
+									},
+									Tokens: tk[2:3],
+								},
+								Tokens: tk[2:3],
+							},
+							Tokens: tk[2:3],
+						},
+						Tokens: tk[2:3],
+					},
+					Tokens: tk[2:3],
+				},
+				Tokens: tk[:3],
+			}
+		}},
+		{"a %abc% b", func(t *test, tk Tokens) { // 5
+			t.Output = PipeOrSpecialExpression{
+				SequenceExpression: SequenceExpression{
+					UnaryExpression: UnaryExpression{
+						ExponentiationExpression: ExponentiationExpression{
+							SubsetExpression: SubsetExpression{
+								ScopeExpression: ScopeExpression{
+									IndexOrCallExpression: IndexOrCallExpression{
+										SimpleExpression: &SimpleExpression{
+											Identifier: &tk[0],
+											Tokens:     tk[:1],
+										},
+										Tokens: tk[:1],
+									},
+									Tokens: tk[:1],
+								},
+								Tokens: tk[:1],
+							},
+							Tokens: tk[:1],
+						},
+						Tokens: tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Operator: &tk[2],
+				PipeOrSpecialExpression: &PipeOrSpecialExpression{
+					SequenceExpression: SequenceExpression{
+						UnaryExpression: UnaryExpression{
+							ExponentiationExpression: ExponentiationExpression{
+								SubsetExpression: SubsetExpression{
+									ScopeExpression: ScopeExpression{
+										IndexOrCallExpression: IndexOrCallExpression{
+											SimpleExpression: &SimpleExpression{
+												Identifier: &tk[4],
+												Tokens:     tk[4:5],
+											},
+											Tokens: tk[4:5],
+										},
+										Tokens: tk[4:5],
+									},
+									Tokens: tk[4:5],
+								},
+								Tokens: tk[4:5],
+							},
+							Tokens: tk[4:5],
+						},
+						Tokens: tk[4:5],
+					},
+					Tokens: tk[4:5],
+				},
+				Tokens: tk[:5],
+			}
+		}},
+	}, func(t *test) (Type, error) {
+		var pe PipeOrSpecialExpression
+
+		err := pe.parse(&t.Tokens)
+
+		return pe, err
+	})
+}
+
 func TestSequenceExpression(t *testing.T) {
 	doTests(t, []sourceFn{
 		{"a", func(t *test, tk Tokens) { // 1
