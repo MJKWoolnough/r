608f009a472a400a7aac5e55a91dd468d0d6a1d8
Michael Woolnough <michael.woolnough@gmail.com>
2024-12-30 14:16:28 +0000

    added test code for print tests

---
+++ format_print_test.go
@@ -0,0 +1,24 @@
+package r
+
+import (
+	"fmt"
+	"testing"
+
+	"vimagination.zapto.org/parser"
+)
+
+func TestPrintSource(t *testing.T) {
+	for n, test := range [...][3]string{} {
+		for m, input := range test {
+			tk := parser.NewStringTokeniser(input)
+
+			if f, err := Parse(&tk); err != nil {
+				t.Errorf("test %d.%d: unexpected error: %s", n+1, m+1, err)
+			} else if simple := fmt.Sprintf("%s", f); simple != test[1] {
+				t.Errorf("test %d.%d.1: expecting output %q, got %q", n+1, m+1, test[1], simple)
+			} else if verbose := fmt.Sprintf("%+s", f); verbose != test[2] {
+				t.Errorf("test %d.%d.2: expecting output %q, got %q", n+1, m+1, test[2], verbose)
+			}
+		}
+	}
+}
