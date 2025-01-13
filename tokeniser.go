0efa97b4075ae5d0d8dc1edca0ef784552c7a4d6
Michael Woolnough <michael.woolnough@gmail.com>
2025-01-13 19:26:23 +0000

    treat newlines in any non-compound grouping as potential whitespace

--- tokeniser.go
+++ tokeniser.go
@@ -80,7 +80,7 @@ func (r *rTokeniser) expression(t *parser.Tokeniser) (parser.Token, parser.Token
 	}
 
 	if t.Accept(lineTerminators) {
-		if len(r.tokenDepth) > 0 && r.tokenDepth[len(r.tokenDepth)-1] == ')' {
+		if len(r.tokenDepth) > 0 && r.tokenDepth[len(r.tokenDepth)-1] != '}' {
 			return t.Return(TokenWhitespaceLineTerminator, r.expression)
 		}
 
