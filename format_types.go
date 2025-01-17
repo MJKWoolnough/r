7696b1e72e368270805863540f91f655f8dec2f7
Michael Woolnough <michael.woolnough@gmail.com>
2025-01-17 21:31:36 +0000

    regen'd

--- format_types.go
+++ format_types.go
@@ -682,6 +682,16 @@ func (f *QueryExpression) printType(w io.Writer, v bool) {
 	} else if v {
 		pp.Print("\nQueryExpression: nil")
 	}
+	pp.Print("\nComments: [")
+
+	ipp := indentPrinter{&pp}
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(&ipp, v)
+	}
+
+	pp.Print("\n]")
 
 	pp.Print("\nTokens: ")
 	f.Tokens.printType(&pp, v)
