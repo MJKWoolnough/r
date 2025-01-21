107eec2056c3a6e64181095b2312960fee5c0fab
Michael Woolnough <michael.woolnough@gmail.com>
2025-01-21 07:47:07 +0000

    regen'd

--- format_types.go
+++ format_types.go
@@ -21,6 +21,16 @@ func (f *AdditionExpression) printType(w io.Writer, v bool) {
 	} else if v {
 		pp.Print("\nAdditionExpression: nil")
 	}
+	pp.Print("\nComments: [")
+
+	ipp := indentPrinter{&pp}
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(&ipp, v)
+	}
+
+	pp.Print("\n]")
 
 	pp.Print("\nTokens: ")
 	f.Tokens.printType(&pp, v)
