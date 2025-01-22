4be400f0d9c0ec1f21ebd7b7be5410f098b2c6ff
Michael Woolnough <michael.woolnough@gmail.com>
2025-01-22 07:51:15 +0000

    regen'd

--- format_types.go
+++ format_types.go
@@ -606,6 +606,16 @@ func (f *MultiplicationExpression) printType(w io.Writer, v bool) {
 	} else if v {
 		pp.Print("\nMultiplicationExpression: nil")
 	}
+	pp.Print("\nComments: [")
+
+	ipp := indentPrinter{&pp}
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(&ipp, v)
+	}
+
+	pp.Print("\n]")
 
 	pp.Print("\nTokens: ")
 	f.Tokens.printType(&pp, v)
